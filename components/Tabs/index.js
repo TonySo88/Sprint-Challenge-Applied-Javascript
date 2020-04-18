245520f7c326756a36fba17be37f91265ef198b9
TonySo88 <60905365+TonySo88@users.noreply.github.com>
2020-04-18 15:00:49 -0500

    changes made

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,30 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+const tabs = document.querySelector(".tabs")
+const topics = document.querySelector(".topics")
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(function (response) {
+    // handle success
+    // const newTab = tabCreator(response.data)
+    let responseVar = response.data.topics
+    responseVar.map((tab) => {
+        const newTab = tabCreator(tab)
+        topics.appendChild(newTab)
+    })
+    console.log(response)
+  })
+  .catch(function (error) {
+    // handle error
+    // console.log(error);
+  })
+
+  const tabCreator = (object) => {
+   const tab = document.createElement("div")
+   tab.textContent = object
+   tab.classList.add('tab')
+    return tab
+  }
+
